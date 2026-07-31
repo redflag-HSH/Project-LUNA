dd6558b4d92c2f4036d60454eea0b6b2776d645d
redflag-HSH <66863292+redflag-HSH@users.noreply.github.com>
2026-07-31 18:41:40 +0900

    Update PlayerBasicStatContainer.cs

--- Assets/00.Scripts/Player/PlayerBasicStatContainer.cs
+++ Assets/00.Scripts/Player/PlayerBasicStatContainer.cs
@@ -4,28 +4,28 @@ public class PlayerBasicStatContainer : MonoBehaviour
 {
     public static PlayerBasicStatContainer Instance { get; private set; }
 
-    //30+N
-    private int _attackPoint;
-    //6+N
-    private int _defendPoint;
-    //100+N
-    private int _HealthPoint;
-    //100+N
-    private int _staminaPoint;
-    //20+0.1*N
-    private int _moveSpeedPoint;
-    //10+0.1*N
-    private int _attackSpeedPoint;
-    //5+0.1*N
-    private int _luckPoint;
+    private int _attackPoint = 30;
+    private int _plusAttackPoint;
+    private int _defendPoint = 6;
+    private int _plusDefendPoint;
+    private int _healthPoint = 100;
+    private int _plusHealthPoint;
+    private int _staminaPoint = 100;
+    private int _plusStaminaPoint;
+    private int _moveSpeedPoint = 20;
+    private int _plusMoveSpeedPoint;
+    private int _attackSpeedPoint = 10;
+    private int _plusAttackSpeedPoint;
+    private int _luckPoint = 5;
+    private int _plusLuckPoint;
 
-    public int AttackPoint => _attackPoint;
-    public int DefendPoint => _defendPoint;
-    public int HealthPoint => _HealthPoint;
-    public int StaminaPoint => _staminaPoint;
-    public int MoveSpeedPoint => _moveSpeedPoint;
-    public int AttackSpeedPoint => _attackSpeedPoint;
-    public int LuckPoint => _luckPoint;
+    public int AttackPoint => _attackPoint + _plusAttackPoint;
+    public int DefendPoint => _defendPoint + _plusDefendPoint;
+    public int HealthPoint => _healthPoint + _plusHealthPoint;
+    public int StaminaPoint => _staminaPoint + _plusStaminaPoint;
+    public int MoveSpeedPoint => _moveSpeedPoint + (int)(_plusMoveSpeedPoint * .1f);
+    public int AttackSpeedPoint => _attackSpeedPoint + (int)(_plusAttackSpeedPoint * .1f);
+    public int LuckPoint => _luckPoint + (int)(_plusLuckPoint * .1f);
 
     void Awake()
     {
